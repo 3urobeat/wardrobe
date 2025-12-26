1f27e10e9f985df30ecc974102ee6c387b273896
3urobeat <tomke@goritz.org>
2025-12-26 21:40:48 +0100

    feat: Support starting & ending lists in getLabelOrderIndexBetween(), make sure list starts at 1.0 in getNewLastLabelOrderIndex() and add helper for sorting a labels array

--- src/model/label.ts
+++ src/model/label.ts
@@ -4,7 +4,7 @@
  * Created Date: 2025-09-09 21:59:50
  * Author: 3urobeat
  *
- * Last Modified: 2025-12-26 15:11:06
+ * Last Modified: 2025-12-26 20:52:24
  * Modified By: 3urobeat
  *
  * Copyright (c) 2025 3urobeat <https://github.com/3urobeat>
@@ -30,10 +30,10 @@ export type Label = {
 /**
  * Helper: Gets a new last orderIndex for (unsorted) list of labels
  * @param list List of labels to analyze
- * @returns {number} New last orderIndex floating point number. If list is empty, 0.0 will be returned.
+ * @returns {number} New last orderIndex floating point number. If list is empty, 1.0 will be returned.
  */
 export function getNewLastLabelOrderIndex(list: Label[]) {
-    if (!list || list.length == 0) return 0.0;
+    if (!list || list.length == 0) return 1.0; // Important to use 1.0 to allow labels to be placed infront later on
 
     // Quickly find the currently greatest orderIndex
     let thisOrderIndex = list[0]!.orderIndex;
@@ -50,14 +50,27 @@ export function getNewLastLabelOrderIndex(list: Label[]) {
 
 /**
  * Helper: Calculates orderIndex to position new element between two existing elements
- * @param el1 Preceding list element
- * @param el2 Succeeding list element
+ * @param el1 Preceding list element. If `undefined`, 0.0 will be used for calculation.
+ * @param el2 Succeeding list element. If `undefined`, a new succeeding label order index will be returned.
  * @returns {number} Floating point number half way between el1 & el2
  */
-export function getLabelOrderIndexBetween(el1: Label, el2: Label) {
-    if (!el1 || !el2) throw("Parameters el1 & el2 must not be undefined");
+export function getLabelOrderIndexBetween(el1?: Label, el2?: Label) {
+    const el1Index = el1 ? el1.orderIndex : 0.0;
 
-    return (el1.orderIndex + el2.orderIndex) / 2;
+    if (!el2) {
+        return Math.trunc(el1Index) + 1.0;
+    }
+
+    return (el1Index + el2.orderIndex) / 2;
+}
+
+/**
+ * Helper: Sorts a list of labels by their orderIndex in ascending order
+ * @param list List of labels to analyze
+ * @returns {Labels[]} Sorted array of labels in ascending order
+ */
+export function sortLabelsList(list: Label[]) {
+    return list.sort((a, b) => a.orderIndex - b.orderIndex);
 }
 
 
